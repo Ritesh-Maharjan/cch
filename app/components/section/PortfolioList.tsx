bafe9b077aac10a9cf98afdb21e16e482ef23687
Ritesh <riteshmaharjan07@gmail.com>
2026-03-25 07:35:00 -0700

    Added mobile view and animation

---
+++ app/components/section/PortfolioList.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { PortfolioItem } from "@/lib/wordpress";
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+const container = {
+  hidden: {},
+  show: {
+    transition: {
+      staggerChildren: 0.15,
+    },
+  },
+};
+
+const item = {
+  hidden: { opacity: 0, y: 40 },
+  show: { opacity: 1, y: 0 },
+};
+
+const PortfolioList = ({ data }: { data: PortfolioItem[] }) => {
+  return (
+    <motion.section className="flex flex-col">
+      {data.map((el) => (
+        <motion.div
+          key={el.id}
+          className="md:border-2 group md:hover:text-black md:hover:bg-white transition-all duration-500 ease-in min-h-60 flex flex-col md:flex-row md:gap-20"
+        >
+          <div className="relative w-full h-100 md:w-113 md:h-auto shrink-0 overflow-hidden">
+            <Image
+              className="
+                object-cover grayscale-100
+                md:group-hover:grayscale-0 md:group-hover:scale-105
+                transition-all duration-500 ease-in
+              "
+              src={el.acf.cover_image}
+              alt={el.title.rendered}
+              fill
+            />
+          </div>
+
+          <motion.div
+            variants={item}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: true, margin: "-100px" }}
+            transition={{ duration: 0.6, ease: "easeOut" }}
+            className="py-10 md:py-0 px-4.5 self-center flex flex-col gap-4"
+          >
+            <h2 className="text-2xl font-heading">{el.title.rendered}</h2>
+            <p className="md:max-w-[70ch]">{el.acf.description}</p>
+          </motion.div>
+        </motion.div>
+      ))}
+    </motion.section>
+  );
+};
+
+export default PortfolioList;
--- app/portfolios/page.tsx
+++ app/portfolios/page.tsx
@@ -1,38 +1,25 @@
 import { getPortfolios } from "@/lib/wordpress";
 import Header from "../components/layout/Header";
-import Image from "next/image";
+import PortfolioList from "../components/section/PortfolioList";
 
 const page = async () => {
   const data = await getPortfolios();
   return (
     <div className="bg-[#194B75] min-h-screen w-screen pt-0.5">
-      <section className="min-mx-4 max-w-312.5 mx-auto">
+      <section className="mx-4 max-w-312.5 xl:mx-auto z-10 relative">
         <Header />
         <div className="mt-20 mb-6 flex flex-col gap-10">
-          <h1 className="font-heading text-xl font-bold">Capital Portfolio</h1>
-          <p className="text-5xl font-light">Partners in Performance</p>
+          <h1 className="font-heading text-lg md:text-xl font-bold">
+            Capital Portfolio
+          </h1>
+          <p className="text-2xl md:text-5xl font-light">
+            Partners in Performance
+          </p>
         </div>
       </section>
 
-      <section className="flex flex-col">
-        {data.map((el) => (
-          <div className="border-2 group hover:text-black hover:bg-white transition-all duration-500 delay-100 ease-in min-h-60 flex gap-20" key={el.id}>
-            <div className="relative w-113 h-auto shrink-0 overflow-hidden">
-              <Image
-                className="object-cover grayscale-100 group-hover:grayscale-0 group-hover:scale-105 transition-all duration-500 delay-200 ease-in"
-                src={el.acf.cover_image}
-                alt={`${el.title.rendered}`}
-                fill
-              />
-              {/* <Image className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 hidden group-hover:block" src={el.acf.project_logo} alt="Logo" height={150} width={150} /> */}
-            </div>
-            <div className="self-center flex flex-col gap-4">
-              <h2 className="text-2xl font-heading transition-all duration-500 delay-100">{el.title.rendered}</h2>
-              <p className="w-[70ch] transition-all duration-500 delay-100">{el.acf.description}</p>
-            </div>
-          </div>
-        ))}
-      </section>
+
+      <PortfolioList data={data} />
     </div>
   );
 };
