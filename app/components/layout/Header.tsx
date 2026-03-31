604443553dc6b3a804908c63e18d7182b5226d2c
Ritesh <riteshmaharjan07@gmail.com>
2026-03-31 07:04:02 -0700

    Made headeer fixed

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import Nav from "./Nav";
 
 const Header = () => {
   return (
-    <header className="z-50 flex items-center justify-between mt-8 w-full max-w-312.5 mx-auto">
+    <header className="fixed top-8 z-50 flex items-center justify-between w-full max-w-312.5 px-4 left-0 right-0">
       <div>
         <Image
           className="hidden w-40 lg:w-56 md:block "
